6d751dc9842bf530e31c1129e82aa762fa8e4828
qadir07 <bugtiqadir77@gmail.com>
2024-12-10 16:48:30 +0500

    My First Website

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,15 @@
+// nav bar
+function Navbar(){
+    return(
+        <>
+        <div class="navbar">
+  <a href="home.html"><i className="fa fa-fw fa-user"></i> home</a>
+  <a className="active" href="project.html"><i className="fa fa-fw fa-home"></i> project</a>
+  <a href="Image .html"><i className="fa fa-fw fa-search"></i> image</a>
+  <a href="contact.html"><i className="fa fa-fw fa-envelope"></i> Contact</a>
+  <a href="login.html"><i className="fa fa-fw fa-user"></i> Login</a>
+</div>
+        </>
+    )
+}
+export default Navbar;
---
+++ src/Component/Wellcome.jsx
@@ -0,0 +1,30 @@
+//welcome
+function Wellcome() {
+  return (
+  <>
+      <div className="header">
+        <h1>This is my first website</h1>
+        <h2>A website created by me.</h2>
+      </div>
+      <div className="row">
+        <div className="side">
+          <h2>About Me</h2>
+          <h5>Photo of me:</h5>
+        </div>
+      </div>
+      <h4> My name is qadir i am from pakistan</h4>
+      <p>My Worker</p>
+     <h3>Zubair</h3>
+      <h3>Umer</h3>
+          <h3>Shafiq</h3>
+      
+    
+      
+      <h2>Title Heading</h2>
+      <h5>Description,</h5>
+      <h4>I am software developer who is adept at web and mobile application development. He has good experience with technologies such as JavaScript, CSS, and React. He is an expert in problem-solving and completes projects using agile methodologies. Qadir has worked on several successful software projects, implementing user-friendly interfaces and scalable back-end systems. His goal is to provide high-quality solutions to every project on time and within budget.</h4>
+      
+      
+    </>
+  );
+}
